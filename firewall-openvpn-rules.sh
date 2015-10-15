9c1bb61cca69e825e0eece87b67fb7df4d849516
jmcombs <jmcombs@me.com>
2015-10-15 09:40:58 -0400

    Update firewall-openvpn-rules.sh

--- firewall-openvpn-rules.sh
+++ firewall-openvpn-rules.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j SNAT --to-source 192.168.1.1 # Inside IP Address of Raspberry Pi
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j SNAT --to-source 192.168.75.1 #IP ADDRESS OF RASPBERRY PI
